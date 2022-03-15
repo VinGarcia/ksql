1d396f8cacedef2ff714f3440fad2370f3be1062
Vinícius Garcia <vingarcia00@gmail.com>
2022-03-15 20:55:18 -0300

    Fix linter complaints

--- adapters/kpgx/kpgx.go
+++ adapters/kpgx/kpgx.go
@@ -6,6 +6,8 @@ import (
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/vingarcia/ksql"
 
+	// This is imported here so the user don't
+	// have to worry about it when he uses it.
 	_ "github.com/lib/pq"
 )
 
