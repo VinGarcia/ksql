eb46b454cbe09f66584912b2d7263be59c175560
Vinícius Garcia <vingarcia00@gmail.com>
2022-03-15 21:01:14 -0300

    Simplify run-all-tests script

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -3,6 +3,4 @@
 # Generate the coverate.txt file for all modules:
 find . -name go.mod -execdir go test -coverprofile=coverage.txt -covermode=atomic -coverpkg=github.com/vingarcia/ksql ./... \;
 
-# Merge all coverage files:
-echo 'mode: atomic' > coverage.txt
-find . -name coverage.txt -exec cat partial-coverage.txt \; | grep -v 'mode: atomic' | sort >> coverage.txt
+# codecov will find all `coverate.txt` files, so it will work fine.
